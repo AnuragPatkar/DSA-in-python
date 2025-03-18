e1b27f60ae367f8b1321c07c49a33ce3113e9237
AnuragPatkar <anuragpatkar786@gmail.com>
2025-03-18 14:36:03 +0530

    add

--- 14 deque using list/deque_using_extending_list.py
+++ 14 deque using list/deque_using_extending_list.py
@@ -1,44 +1,41 @@
-class deque:
-    def __init__(self):
-        self.mylist=[]
-    
+class Deque(list):
     def is_empty(self):
-        return len(self.mylist)==0
+        return len(self)==0
     
     def size(self):
-        return len(self.mylist)
+        return len(self)
     
     def insert_front(self,data):
-        self.mylist.insert(0,data)
+        self.insert(0,data)
 
     def insert_rear(self,data):
-        self.mylist.append(data)
+        self.append(data)
 
     def delete_front(self):
         if self.is_empty():
             raise IndexError("Deque is empty")
         else:
-            self.mylist.pop(0)
+            self.pop(0)
     
     def delete_rear(self):
         if self.is_empty():
             raise IndexError("Deque is empty")
         else:
-            self.mylist.pop(-1)
+            self.pop(-1)
 
     def get_front(self):
         if self.is_empty():
             raise IndexError("Deque is empty")
         else:
-            return self.mylist[0]
+            return self[0]
         
     def get_rear(self):
         if self.is_empty():
             raise IndexError("Deque is empty")
         else:
-            return self.mylist[-1]
+            return self[-1]
 
-d1=deque()
+d1=Deque()
 print(d1.size())
 print(d1.is_empty())
 d1.insert_front(10)        
@@ -54,7 +51,4 @@ d1.delete_front()
 d1.delete_rear()
 print(d1.get_front())        
 print(d1.get_rear())
-print(d1.size())        
-
-
-    
+print(d1.size()) 
--- 14 deque using list/question.py
+++ 14 deque using list/question.py
@@ -1,19 +1,50 @@
 # Assignment-14: Deque Implementation using list
 
-# 1. Define a class Deque to implement deque data structure using list. Define init() method to create an empty list object as instance object member of Deque.
+""" # 1. Define a class Deque to implement deque data structure using list. Define init() method to create an empty list object as instance object member of Deque.
+class deque:
+    def __init__(self):
+        self.mylist=[] """
 
-# 2. Define a method is_empty() to check if the deque is empty in Deque class.
+""" # 2. Define a method is_empty() to check if the deque is empty in Deque class.
+    def is_empty(self):
+        return len(self.mylist)==0 """
 
-# 3. In Deque class, define insert_front() method to add data at the front end of the deque.
+""" # 3. In Deque class, define insert_front() method to add data at the front end of the deque.
+    def insert_front(self,data):
+        self.mylist.insert(0,data) """
 
-# 4. In Deque class, define insert_rear() method to add data at the rear end of the deque.
+""" # 4. In Deque class, define insert_rear() method to add data at the rear end of the deque.
+    def insert_rear(self,data):
+        self.mylist.append(data) """
 
-# 5. In Deque class, define delete_front() method to remove front element from the deque.
+""" # 5. In Deque class, define delete_front() method to remove front element from the deque.
+    def delete_front(self):
+        if self.is_empty():
+            raise IndexError("Deque is empty")
+        else:
+            self.mylist.pop(0) """
 
-# 6. In Deque class, define delete_rear() method to remove rear element from the deque.
+""" # 6. In Deque class, define delete_rear() method to remove rear element from the deque.
+    def delete_rear(self):
+        if self.is_empty():
+            raise IndexError("Deque is empty")
+        else:
+            self.mylist.pop(-1) """
 
-# 7. In Deque class, define get_front() method to return front element of the deque.
+""" # 7. In Deque class, define get_front() method to return front element of the deque.
+    def get_front(self):
+        if self.is_empty():
+            raise IndexError("Deque is empty")
+        else:
+            return self.mylist[0] """
 
-# 8. In Deque class, define get_rear() method to return rear element of the deque.
+""" # 8. In Deque class, define get_rear() method to return rear element of the deque.
+    def get_rear(self):
+        if self.is_empty():
+            raise IndexError("Deque is empty")
+        else:
+            return self.mylist[-1] """
 
-# 9. In Deque class, define size() method to return size of the deque that is number of items present in the deque
+""" # 9. In Deque class, define size() method to return size of the deque that is number of items present in the deque
+    def size(self):
+        return len(self.mylist) """
