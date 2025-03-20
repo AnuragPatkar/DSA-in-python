610ee57bde44aaff350d49832cfee4e3f39d1cb7
AnuragPatkar <anuragpatkar786@gmail.com>
2025-03-20 10:35:09 +0530

    add

--- 27 quick sort/question.py
+++ 27 quick sort/question.py
@@ -1,3 +1,30 @@
 #Assignment-27: Quick Sort
 
 #!1. Write a python function to implement quick sort
+def Quick_sort(list1,left,right):
+    if left<right:
+        m=Quick_process(list1,left,right)
+        Quick_sort(list1,left,m-1)
+        Quick_sort(list1,m+1,right)
+
+def Quick_process(list1,left,right):
+    mid=left
+    while True:
+        if left==right:
+            break
+        while list1[mid]<list1[right]:
+            right-=1
+        if list1[mid]>list1[right]:
+            list1[mid],list1[right]=list1[right],list1[mid]
+            mid=right
+        while list1[mid]>list1[left]:
+            left+=1
+        if list1[mid]<list1[left]:
+            list1[mid],list1[left]=list1[left],list1[mid]
+            mid=left
+    return mid
+    
+
+list1=[50,55,30,45,40,32,52,60,99,75]
+Quick_sort(list1,0,len(list1)-1)
+print(list1)
--- 28 merge sort/question.py
+++ 28 merge sort/question.py
@@ -1,3 +1,36 @@
 # Assignment-28: Merge Sort
 
 #! 1. Write a python function to implement Merge sort
+def merge_sort(mylist):
+    if len(mylist)>1:
+        mid=len(mylist)//2
+        leftlist=mylist[:mid]
+        rightlist=mylist[mid:]
+    
+        merge_sort(leftlist)
+        merge_sort(rightlist)
+
+        i=j=k=0
+        while i<len(leftlist) and j<len(rightlist):
+            if leftlist[i]<rightlist[j]:
+                mylist[k]=leftlist[i]
+                i+=1
+            else:
+
+                mylist[k]=rightlist[j]
+                j+=1
+            k+=1
+
+        while i<len(leftlist):
+            mylist[k]=leftlist[i]
+            i+=1
+            k+=1
+
+        while j<len(rightlist):
+            mylist[k]=rightlist[j]
+            j+=1
+            k+=1
+
+mylist=[55,30,45,60,80,65,75,25]
+merge_sort(mylist)
+print(mylist)
